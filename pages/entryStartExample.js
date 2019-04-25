3b6a8f24c312741a3e4efc0a4eb570fa64f1e098
omo <979973510@qq.com>
2019-04-25 19:43:59 +0800

    0425 config info

--- pages/entryStartExample.js
+++ pages/entryStartExample.js
@@ -1,11 +1,11 @@
 // 入口文件，各个页面组件等的依赖关系都在此文件明确
 //  注册组件等
 
-var start = document.createElement('div');
-start.innerText = "start your journey";
-start.style.cssText = "text-align:center";
-
-document.body.append(start);
+// var start = document.createElement('div');
+// start.innerText = "start your journey";
+// start.style.cssText = "text-align:center";
+//
+// document.body.append(start);
 
 console.log('This a start');
 console.log('enjoy');
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const path = require('path');
+// const webpack = require('webpack');
+// 自动生成html，并将打包后js插入该html
 const htmlWebpackPlugin = require('html-webpack-plugin');
+// 单独提取出css
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// 单独提取出图片
+// 把公共部门提取出来
+// css类名混淆
+
 
 module.exports = {
     entry: "./pages/entry.js",
+    // entry:{
+    //     app: "./pages/entry.js",
+    //     vendors: ['vue']
+    // },
     // output:   __dirname + "/build/bundle.js",
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'build.js'
+        filename: 'dede.js'
     },
     devServer: {
         contentBase: path.join(__dirname, 'build'),
@@ -16,13 +28,20 @@ module.exports = {
     // devtool: "source-map",
     module:{
         rules: [
+            // {
+            //     test: /\.css$/,
+            //     use: [{
+            //         loader: "style-loader"
+            //     }, {
+            //         loader: "css-loader"
+            //     }]
+            // },
             {
                 test: /\.css$/,
-                use: [{
-                    loader: "style-loader"
-                }, {
-                    loader: "css-loader"
-                }]
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
             },
             {
                 test: /(\.jsx|\.js)$/,
@@ -59,7 +78,7 @@ module.exports = {
         fs: 'empty'
     },
     resolve: {
-        extensions: [ '.js', '.vue'],
+        extensions: ['.js', '.vue',  '.json'],
         alias: {
             'vue$': 'vue/dist/vue.js',
             'pages': path.resolve(__dirname, './pages'),
@@ -67,9 +86,11 @@ module.exports = {
         }
     },
     plugins:[
+        // new webpack.optimize.CommonsChunkPlugin({name: 'vendors', filename: 'vendors'})
         new htmlWebpackPlugin({
             filename: 'index.html',
             template: 'template.html'
-        })
+        }),
+        new ExtractTextPlugin("style.css")
     ]
 }
