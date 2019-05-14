3b946f0bd76c47d69226fc5f73b893462740db1d
omo <979973510@qq.com>
2019-05-14 18:20:22 +0800

    echartDemo init

--- pages/page/echartDemo.vue
+++ pages/page/echartDemo.vue
@@ -3,7 +3,8 @@
         <div
                 ref='barchart'
                 class='echartbox'
-        >dsd
+                style="width:300px;height:300px"
+        >
         </div>
     </div>
 </template>
@@ -56,101 +57,22 @@
         },
         methods: {
             drawBar () {
-                let myCharts = Echarts.init(this.$refs.barchart)
+                debugger;
+                let myCharts = Echarts.init(this.$refs.barchart);
                 myCharts.setOption({
-                    color: ['#83d0d5', '#f1cb48', '#188ae2', '#E8830B', '#7460ae', '#fc4b6c', '#31ce77', '#eae0bc', '#e732cb', '#9dce8a'],
-                    title: {
-                        text: this.title,
-                        left: '35',
-                        top: '20',
-                        link: this.link,
-                        textStyle: {
-                            color: '#B6B6B6'
-                        }
+
+                    xAxis: {
+                        type: 'category',
+                        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
                     },
-                    tooltip: {
-                        trigger: 'axis',
-                        axisPointer: {
-                            type: 'shadow'
-                        }
+                    yAxis: {
+                        type: 'value'
                     },
-                    grid: {
-                        left: '15%',
-                        bottom: '30%'
-                    },
-                    legend: {
-                        data: this.legend_data,
-                        left: 'center',
-                        bottom: '0',
-                        textStyle: {
-                            color: '#B6B6B6'
-                        }
-                    },
-                    xAxis: [
-                        {
-                            type: 'category',
-                            position: 'bottom',
-                            name: this.x_name,
-                            nameLocation: 'center',
-                            nameGap: '50',
-                            data: this.axis_data,
-                            axisTick: {
-                                show: false
-                            },
-                            axisLabel: {
-                                textStyle: {
-                                    color: '#BFC2C8'
-                                },
-                                rotate: 30,
-                                interval: 0
-                            },
-                            axisLine: {
-                                lineStyle: {
-                                    color: '#BFC2C8'
-                                }
-                            }
-                        }
-                    ],
-                    yAxis: [
-                        {
-                            type: 'value',
-                            left: '10',
-                            name: this.y_name,
-                            nameLocation: 'center',
-                            nameGap: '45',
-                            axisLine: {
-                                lineStyle: {
-                                    color: '#BFC2C8'
-                                }
-                            },
-                            axisTick: {
-                                show: false
-                            },
-                            splitLine: {
-                                lineStyle: {
-                                    color: '#898D95'
-                                }
-                            },
-                            axisLabel: {
-                                textStyle: {
-                                    color: '#BFC2C8'
-                                },
-                                interval: 0,
-                                formatter: function (value, index) {
-                                    if (value >= 1000 && value < 10000000) {
-                                        value = value / 1000 + 'K'
-                                    } else if (value >= 1000000 && value < 1000000000) {
-                                        value = value / 1000000 + 'M'
-                                    } else if (value >= 1000000000) {
-                                        value = value / 1000000000 + 'B'
-                                    }
-                                    return value
-                                }
-                            }
-                        }
-                    ],
-                    series: this.series_data
-                })
+                    series: [{
+                        data: [820, 932, 901, 934, 1290, 1330, 1320],
+                        type: 'line'
+                    }]
+                });
                 window.addEventListener('resize', function () {
                     myCharts.resize()
                 })
