9a25343d71c7ee8cbf2d5fb7e78221da490aa20e
omo <979973510@qq.com>
2019-11-04 19:18:47 +0800

    init components

---
+++ src/components/components.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+function formatName(str){
+    return str.charAt(0).toUpperCase() + str.slice(1)
+}
+var components = require.context('./', true, /\.vue$/)
+components.keys().forEach((key) => {
+    const config = components(key)
+    const name = formatName(key.replace(/^\.\//, '').replace(/\.\w+$/, ''))
+    Vue.component(name, config.default || config )
+})
