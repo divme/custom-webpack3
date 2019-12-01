a7e048ad965f8e0b0a16399d1f2836b8f343276e
omohome <979973510@qq.com>
2019-12-01 23:00:09 +0800

    commit comments

--- webpack.profile.explain.js
+++ webpack.profile.explain.js
@@ -1,3 +1,14 @@
+// git
+// git checkout -b localname  origin/remoteBranch  从远程仓库检出分支到本地
+// git push --set-upstream origin  localname  将本地分支推送到远程仓库
+// git push origin localname:remotename 将本地分支推送到远程分支
+// git push origin :remotename 利用推送方法，删除远程分支
+// git push origin --delete remotebranch 删除远程分支
+
+// git branch -r -D origin/branchname 删除本地的远程分支信息： 本地分支存在，远程分支也存在，但是本地不再有此远程分支的信息
+// git branch --set-upstream-to origin/branchname  将当前分支关联到远程分支
+
+
 // webpack 路径
 // 模块路径 node_modules
 // import 'vue'
